740b56f7351d8c1174a8d9b874c823ef951a61cd
“mahadev91op” <mahadevtanti191@gmail.com>
2025-12-21 10:42:14 +0530

    about section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import HeroSection from "../components/homepage/HeroSection";
 import Features from "../components/homepage/Features";
-import StudentReviews from "../components/homepage/StudentReviews"; // Naya Import
+import AboutSection from "../components/homepage/AboutSection"; // <-- Import kiya
+import StudentReviews from "../components/homepage/StudentReviews";
 
 export default function Home() {
   return (
     <main className="bg-black min-h-screen">
       <HeroSection />
+      
+      {/* Features ke baad About Section add kiya */}
       <Features />
+      <AboutSection /> 
+      
       <StudentReviews />
     </main>
   );
